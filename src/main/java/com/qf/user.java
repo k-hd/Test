d923b7d7f31c0ef7f0aa197ec5c0d5ca30b4f96d
k-hd <247369554@qq.com>
2020-08-04 19:04:13 +0800

    首次测试....

--- src/main/java/com/qf/user.java
+++ src/main/java/com/qf/user.java
@@ -3,5 +3,6 @@ package com.qf;
 public class user {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈");
+        System.out.println("safbhjdsbfkjdsfbdjks");
     }
 }
